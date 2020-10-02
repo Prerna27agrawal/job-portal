d3e81611386fab14d940723214a9afd92db7dfd6
Agarwal Neetu <agarwalneetu301@gmail.com>
2020-10-02 16:07:05 +0530

    added color route

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
-app.set("view engine","ejs");
-app.get("/",function(req,res){
-    res.render("colourgame");
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+  res.render("colourgame");
 });
-var port=process.env.PORT || 3000;
-app.listen(port,function()
-		  {
-	console.log("Game has started");
+app.get("/color", function (req, res) {
+  res.send("Pick a color");
+});
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Game has started");
 });
