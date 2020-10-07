ab68386c99b5802bf42a3bb9587cd12f1bbb6b45
Prerna27agrawal <agrawalprerna1999@gmail.com>
2020-10-08 01:35:54 +0530

    rearranged app.js and register.ejs of company

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ var Job = require("./models/job");
 var Company= require("./models/company");
 var Seeker = require("./models/seeker");
 
+app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
 // accept json 
 app.use(bodyParser.json());
 
@@ -20,19 +21,14 @@ const mongoose = require("mongoose");
 const company = require("./models/company");
 mongoose.connect("mongodb://localhost:27017/job_portal");
 
-app.set("view engine", "ejs");
-
 //Image Upload- Mutler
 // var fs = require('fs'); 
 // var path = require('path');
 // var multer = require('multer');  
 // require('dotenv/config');
-
-
 // var fs = require('fs'); 
 // var path = require('path'); 
 // var multer = require('multer'); 
-  
 // var storage = multer.diskStorage({ 
 //     destination: (req, file, cb) => { 
 //         cb(null, 'uploads') 
@@ -41,7 +37,6 @@ app.set("view engine", "ejs");
 //         cb(null, file.fieldname + '-' + Date.now()) 
 //     } 
 // }); 
-  
 // var upload = multer({ storage: storage }); 
 
 
@@ -50,22 +45,26 @@ app.set("view engine", "ejs");
 app.get("/", function (req, res) {
   res.render("landing");
 });
+
 app.get("/login", function (req, res) {
   res.render("login");
 });
+
 app.get("/login/company", function (req, res) {
   res.render("company/companylogin");
-})
+});
+
 app.get("/login/seeker", function (req, res) {
   res.render("seeker/seekerlogin");
-})
+});
 
 app.get("/register/company", function (req, res) {
   res.render("company/companyregister");
-})
+});
+
 app.get("/register/seeker", function (req, res) {
   res.render("seeker/seekerregister");
-})
+});
 
 app.get("/register", function (req, res) {
   res.render("register");
@@ -74,7 +73,8 @@ app.get("/register", function (req, res) {
 app.get("/login/seeker/companyname", function (req, res) {
   res.send("let us apply to my company and work ");
 });
-//jb company login kre toh usko job create kkre id mil jae compnay ki
+
+//jb company login kre toh usko job create karkee id mil jae company ki
 app.get("/login/company/:id/createjob",function(req,res){
   Company.findById(req.params.id,function(err,company){
     if(err)
@@ -82,12 +82,10 @@ app.get("/login/company/:id/createjob",function(req,res){
     else
     res.render("company/createjob",{Company: company});
   });
- // res.render("company/createjob");
 });
 
 //saari jobs uss comapny ki show hongi
 app.get("/login/company/:id/viewjob",function(req,res){
-
   Company.findById(req.params.id).populate("jobs").exec(function(err,foundcom){
     if(err)
     console.log(err);
@@ -97,40 +95,43 @@ app.get("/login/company/:id/viewjob",function(req,res){
       res.render("company/viewjob",{Company: foundcom});
     }
   }); 
- // res.send("following are the jobs");
 });
+
+
 
 //POST Request
 app.post("/login/seeker", function (req, res) {
   res.render("seeker/index");
 });
+
 //company register page 
 app.post("/register/company", function (req, res) {
-  var name=req.body.name;
-  var email=req.body.email;
-  var tagline=req.body.tagline;
-  var description=req.body.description;
-  var logo= req.body.logo;
-  var password =req.body.password;
-  var newCompany = new Company({
-    name,
-    email,
-    tagline,
-    description,
-    logo,
-    password
-  });
-  Company.create(newCompany,function(err,newcompanycreate){
+  //not useful as we already used newCompany in our register form to use as array
+  // var name=req.body.name;
+  // var email=req.body.email;
+  // var tagline=req.body.tagline;
+  // var description=req.body.description;
+  // var logo= req.body.logo;
+  // var password =req.body.password;
+  // var newCompany = new Company({
+  //   name,
+  //   email,
+  //   tagline,
+  //   description,
+  //   logo,
+  //   password
+  // });
+  Company.create(req.body.newCompany,function(err,newcompanycreate){
     if(err) 
     console.log(err);
     else
     {
-      //res.redirect()
        res.render("company/companylogin",{Company: newcompanycreate});
     }
   });
-  console.log(newCompany);
+ // console.log(newCompany);
 });
+
 
 app.post("/login/company/:id", function (req, res) {
   Company.findById(req.params.id,function(err,company){
@@ -140,8 +141,6 @@ app.post("/login/company/:id", function (req, res) {
     res.render("company/companyindex",{Company: company});
   });
 });
-
-
 
 app.post("/register/seeker", function (req, res) {
   res.render("seeker/seekerlogin");
@@ -155,12 +154,6 @@ app.post("/login/company/:id/createjob",function(req,res){
          console.log(err);
     }
     else{
-      // var title=req.body.title;
-      // var company=req.body.company;
-      // var location=req.body.location;
-      // var experience=req.body.experience;
-      // var description= req.body.description;
-      // var newJob = new Job({title,company,location,experience,description});
       Job.create(req.body.job,function(err,job){
         if(err)
         console.log(err);
@@ -174,8 +167,10 @@ app.post("/login/company/:id/createjob",function(req,res){
     });
   }
   });
-  //res.send("job created");
 });
+
+
+
 
 
 var port = process.env.PORT || 3000;
