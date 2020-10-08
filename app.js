78d21b99fbde737223669d5d972eadb9d597f689
Prerna27agrawal <agrawalprerna1999@gmail.com>
2020-10-08 15:12:52 +0530

    changes image file with cloudinary and multer and hidden env file

--- app.js
+++ app.js
@@ -3,11 +3,45 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 
+//for keeping the cloud api secret
+//https://www.npmjs.com/package/dotenv
+require('dotenv').config()
 
 //Model
 var Job = require("./models/job");
 var Company= require("./models/company");
 var Seeker = require("./models/seeker");
+
+
+//for image handling
+
+var multer = require('multer');
+var storage = multer.diskStorage({
+	  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+var cloudinary = require('cloudinary');
+cloudinary.config({ 
+  cloud_name: 'dhr7wlz2k', 
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+
+
+
+
 
 app.set("view engine", "ejs");
 
@@ -20,26 +54,6 @@ app.use(bodyParser.json());
 const mongoose = require("mongoose");
 const company = require("./models/company");
 mongoose.connect("mongodb://localhost:27017/job_portal");
-
-//Image Upload- Mutler
-// var fs = require('fs'); 
-// var path = require('path');
-// var multer = require('multer');  
-// require('dotenv/config');
-// var fs = require('fs'); 
-// var path = require('path'); 
-// var multer = require('multer'); 
-// var storage = multer.diskStorage({ 
-//     destination: (req, file, cb) => { 
-//         cb(null, 'uploads') 
-//     }, 
-//     filename: (req, file, cb) => { 
-//         cb(null, file.fieldname + '-' + Date.now()) 
-//     } 
-// }); 
-// var upload = multer({ storage: storage }); 
-
-
 
 //GET Request
 app.get("/", function (req, res) {
@@ -105,31 +119,28 @@ app.post("/login/seeker", function (req, res) {
 });
 
 //company register page 
-app.post("/register/company", function (req, res) {
-  //not useful as we already used newCompany in our register form to use as array
-  // var name=req.body.name;
-  // var email=req.body.email;
-  // var tagline=req.body.tagline;
-  // var description=req.body.description;
-  // var logo= req.body.logo;
-  // var password =req.body.password;
-  // var newCompany = new Company({
-  //   name,
-  //   email,
-  //   tagline,
-  //   description,
-  //   logo,
-  //   password
-  // });
-  Company.create(req.body.newCompany,function(err,newcompanycreate){
-    if(err) 
-    console.log(err);
-    else
-    {
-       res.render("company/companylogin",{Company: newcompanycreate});
-    }
+app.post("/register/company", upload.single('logo'), function (req, res) {
+  cloudinary.uploader.upload(req.file.path, function(result) {
+    // add cloudinary url for the image to the campground object under image property
+    req.body.newCompany.logo = result.secure_url;
+    Company.create(req.body.newCompany, function(err, newcompanycreate) {
+      if (err) {
+         console.log(err);
+        //req.flash('error', err.message);
+        return res.redirect('back');
+      }
+      else
+          res.render("company/companylogin",{Company: newcompanycreate});
+    });
   });
- // console.log(newCompany);
+//   Company.create(req.body.newCompany,function(err,newcompanycreate){
+//     if(err) 
+//     console.log(err);
+//     else
+//     {
+//        res.render("company/companylogin",{Company: newcompanycreate});
+//     }
+//   });
 });
 
 
